1e52f4210a2ebbfb873fe6a751dc8419e2cdc0b8
SHUAI-12138 <1003110192@qq.com>
2023-08-16 20:29:35 +0800

    Question 29 of leetcode, divide.

---
+++ JavaCode/algorithmquestions/src/com/leetcode/aciDivide/Solution.java
@@ -0,0 +1,34 @@
+package com.leetcode.aciDivide;
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == 0) return 0;
+        if(divisor == 1) return dividend;
+        if(dividend == divisor) return 1;
+        if(dividend == -2147483648 && divisor == -1) return 2147483647;
+        if(divisor == -2147483648) return 0;
+        int flag = -1;
+        if((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+            flag = 1;
+        }
+        if(dividend > 0) dividend = -dividend;
+        if(divisor > 0) divisor = -divisor;
+        int n = 1;
+        int num = divisor;
+        while(num >= (-2147483648 >> 1) && (num << 1) >= dividend) {
+            num <<= 1;
+            n <<= 1;
+        }
+        int res = 0;
+        while(dividend < 0 && num <= divisor) {
+            while(dividend <= num) {
+                dividend -= num;
+                res += n;
+            }
+            if(dividend > divisor) return flag * res;
+            num >>= 1;
+            n >>= 1;
+        }
+        return flag * res;
+    }
+}
